6aadbe0de580c25db12bbfab507cbbeb1bf4519e
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-04-17 15:52:46 +0530

    added missing route pincodes

--- backend/src/routes/pincode.routes.js
+++ backend/src/routes/pincode.routes.js
@@ -10,7 +10,7 @@ router.get("/states/:state/districts/:district/taluks", controller.getTaluks);
 // Search & Filtering
 router.get("/search", controller.searchPincodes);
 router.get("/pincode/:pincode", controller.getPincodeDetail);
-router.get("/", controller.getFilteredPincodes);
+router.get("/pincodes", controller.getFilteredPincodes);
 
 // Analytics
 router.get("/stats", controller.getStats);
